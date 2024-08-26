4990085dbdc912227b6ee0d71846c557954c61fd
Aryan Babber <aryanbabber16@gmail.com>
2024-08-26 18:15:07 +0530

    made changes

--- src/components/mainnav/MainNav.jsx
+++ src/components/mainnav/MainNav.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import {ReactComponent as MainLogo} from "../../assets/logo-main.svg"
-import "./mainnav.css"
+import React from "react";
+import { ReactComponent as MainLogo } from "../../assets/logo-main.svg";
+import "./mainnav.css";
+import { Link } from "react-router-dom";
 
 const MainNav = () => {
-  return (
-    <div>
-        <div className="main-nav">
-          <MainLogo className="logo" />
-          <ul className='nav'>
-            <li>Activities</li>
-            <li>Services</li>
-            <li>Our Accomplishments</li>
-            <li>About us</li>
-          </ul>
+    return (
+        <div>
+            <div className="main-nav">
+                <MainLogo className="logo" />
+                <ul className="nav">
+                    <Link to="/activities">Activities</Link>
+                    <Link to="/services">Services</Link>
+                    <Link to="/accomplishments">Our Accomplishments</Link>
+                    <Link to="/about-us">About us</Link>
+                </ul>
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
 export default MainNav;
