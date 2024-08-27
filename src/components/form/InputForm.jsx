6f9d30cac97598decd20153b66147d48e3bf1da2
Aryan Babber <aryanbabber16@gmail.com>
2024-08-27 11:12:28 +0530

    start of day 1

--- src/components/form/InputForm.jsx
+++ src/components/form/InputForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./inputform.css";
 
 const InputForm = () => {
     const handleSubmit = (e) => {
@@ -6,9 +7,48 @@ const InputForm = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div className=""></div>
-        </form>
+        <div className="contact-us-container">
+            <h1 className="contact-header">Contact us</h1>
+            <form form onSubmit={handleSubmit} className="contact-form">
+                <div className="form-name">
+                    <label className="name-label" htmlFor="name">
+                        Name:
+                    </label>
+                    <input className="input name-input" type="text" name="name" />
+                    <div className="form-name-inner">
+                        <label className="email-label" htmlFor="email">
+                            Email address:
+                        </label>
+                        <br />
+                        <input
+                            className="input email-input"
+                            type="text"
+                            name="email"
+                        />
+                        <br />
+                        <label className="mobile-label" htmlFor="mobile">
+                            Mobile No.:
+                        </label>
+                        <br />
+                        <input
+                            className="input mobile-input"
+                            type="text"
+                            name="mobile"
+                        />
+                    </div>
+                    <label className="message-label" htmlFor="message">
+                        Message:
+                    </label>
+                    <br />
+                    <input
+                        className="input message-input"
+                        type="text"
+                        name="message"
+                    />
+                </div>
+                <button>Send message</button>
+            </form>
+        </div>
     );
 };
 
